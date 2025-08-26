3c2c3f4330de87168b80155f035cc910467c8b68
dylanM127 <0348265@schoolsnet.act.edu.au>
2025-08-26 20:09:10 +1000

    Create I2C LCD progress bar.ino
    Prototype: progress bar for the LCD to be later implemented as a power level.

---
+++ I2C LCD progress bar.ino
@@ -0,0 +1,69 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+byte zero [] = {
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+};
+byte five [] = {
+	B11111,
+	B11111,
+	B11111,
+	B11111,
+	B11111,
+	B11111,
+	B11111,
+	B11111
+};
+
+ int prevVal = 0;
+void setup()
+{
+	Serial.begin(9600);
+	lcd.begin();
+	lcd.backlight();
+
+	lcd.createChar(0, zero);
+	lcd.createChar(5, five);
+}
+
+void loop(){
+	for (int i = 0; i <= 100; i++){
+		lcd.setCursor(0,0);
+		lcd.print(i);
+		lcd.print("%  ");
+		updateBar(i);
+		delay(50);
+		}
+
+	for (int i = 100; i >= 0; i--){
+		lcd.setCursor(0,0);
+		lcd.print(i);
+		lcd.print("%  ");
+		updateBar(i);
+		delay(200);
+	}
+}
+
+void updateBar(unsigned long count){
+	int bar = 0.8*count;
+	int val = bar/5; //givs value between 0 and 16 for the character slot on the disdplay. 
+	//Serial.println (val);
+	int diff = val - prevVal;
+	int prevVal = val;
+	if (diff >= 0){
+	lcd.setCursor(val, 1);
+	lcd.write(5);
+	}
+	
+	lcd.setCursor(val+1, 1);
+	lcd.write(0);
+}
