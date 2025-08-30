a6bb8353932f71c574eefb13aa25b5080de2e9e4
dylanM127 <0348265@schoolsnet.act.edu.au>
2025-08-30 14:47:44 +1000

    Update Servos and joysticks proto.ino

--- Servos and joysticks proto.ino
+++ Servos and joysticks proto.ino
@@ -1,39 +1,59 @@
 #include <Servo.h>
+#include <Stepper.h>
 
-Servo myservoX;
-Servo myservoY;
+Servo myservoX, myservoY;
 
 int valueX = 0;
 int valueY = 0;
+
+const int servoX = 2;
+const int servoY = 3;
+const int button = 40;
+
+const int stepsPerRevolution = 2048;
+
+Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
 
 
 void setup() {
 	//pinMode(A0, INPUT);
 	//pinMode(A1, INPUT);
-  	pinMode(7, INPUT_PULLUP); //set pin 7 as an input and enable the internal pull-up resistor
 	Serial.begin(9600);
 
-  myservoX.attach(3);
-  myservoY.attach(2);
+  myservoX.attach(servoX);
+  myservoY.attach(servoY);
+
+  pinMode(button, INPUT);
 
 }
 
 void loop() {
   valueX = analogRead(A0);	// read X axis value [0..1023]
-  Serial.println("X:");
-  Serial.print(valueX, DEC);
+ // Serial.println("X:");
+  //Serial.print(valueX, DEC);
 
   valueY = analogRead(A1);	// read Y axis value [0..1023]
-  Serial.println(" | Y:");
-  Serial.print(valueY, DEC);
-
- 
+  //Serial.println(" | Y:");
+  //Serial.print(valueY, DEC);
 
   int angleX = map(valueX, 0, 1023, 0, 180);
   myservoX.write(angleX);
 
-  int angleY = map(valueY, 0, 1023, 0, 180);
-  myservoY.write(angleX);
+  //int angleY = map(valueY, 0, 1023, 0, 180);
+  //myservoY.write(angleY);
 
+  int speed = map(valueY, 530, 1023, 0, 15);
+  
+  Serial.println(speed);
+
+  if (speed > 5){
+    myStepper.setSpeed(speed);
+    myStepper.step(100);
+  }
+
+  if (speed < -5){
+    myStepper.setSpeed(-speed);
+    myStepper.step(-100);
+  }
   delay(50);
 }
